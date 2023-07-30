7d0bbdda63ae9776a6f0a55cd184e9e628699e53
gibet <gibet@10.0.0.234>
2023-07-29 23:12:31 -0400

    classes

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -1,5 +1,24 @@
 package day2;
 
 public class DemoDay2 {
+	public static void main(String[] args) {
+		System.out.println("Hi Gibet:Test");
+	}
+	//add method
+	public static void add(int a , int b) {
+		System.out.println(a+b);
+	}
+	//substract
+	public static void substract(int a, int b) {
+		System.out.println(a-b);
+	}
+	//Yasir
+	public static void divede(int a, int b) {
+		System.out.println(a/b);
+	}
+	//add method
+	public static void add(int a , int b,int c) {
+		System.out.println(a+b+c);
 
+}
 }
--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -1,5 +1,9 @@
 package day3;
 
 public class DemoDay3 {
-
+	public static void main(String[] args) {
+		System.out.println("My name is gibet telma");
+		String country = "USA";
+		String state = "VA";
+	}
 }
